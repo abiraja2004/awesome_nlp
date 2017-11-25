9a800e565f9cfe57653d976685396ab8dd3e85df
Verna <verna.dankers@student.uva.nl>
2017-11-25 14:45:49 +0100

    Added beam search

--- NPLM/Decoder.py
+++ NPLM/Decoder.py
@@ -1,12 +1,13 @@
+import torch
+import logging
 from torch.autograd import Variable
 from utils import to_indices
-import torch
 
 
 class Decoder(object):
-    def __init__(self, w2i, i2w, context_size, length):
-        self.word2idx = w2i
-        self.idx2word = i2w
+    def __init__(self, word2idx, idx2word, context_size, length):
+        self.word2idx = word2idx
+        self.idx2word = idx2word
         self.C = context_size
         self.length = length
 
@@ -16,19 +17,20 @@ class Greedy_Decoder(Decoder):
     as the next word."""
     def __init__(self, w2i, i2w, context_size, length):
         super().__init__(w2i, i2w, context_size, length)
+        logging.info("Greedy Decoder initialized.")
 
     def decode(self, sequence, model):
+        """Given a sequence and a model, generate a summary in a
+        greedy manner."""
+
         # Initalize the sentence with enough starting tags
         summary = [self.word2idx['<s>']] * self.C
         sequence_i = Variable(
             torch.LongTensor(to_indices(sequence, self.word2idx))
         )
 
-        i = self.C
-
         # Greedily select the word with the highest probability
-        end_tag = self.word2idx['</s>']
-        while(summary.count(end_tag) < 2 and i < 100):
+        for i in range(self.C, self.length):
             summary_i = Variable(torch.LongTensor(summary[i-self.C:i]))
             scores = model.forward(sequence_i, summary_i)
             prob, index = torch.topk(scores.data, 1)
@@ -42,12 +44,17 @@ class Greedy_Decoder(Decoder):
 
 class Beam_Search_Decoder(Decoder):
     """The Beam Search Decoder maintains K current best hypotheses at
-    the time."""
-    def __init__(self, w2i, i2w, context_size, beam_size, length):
+    a time."""
+    def __init__(self, w2i, i2w, context_size, length, beam_size, verbose=False):
         super().__init__(w2i, i2w, context_size, length)
         self.beam_size = beam_size
+        logging.info("Beam Search Decoder initialized.")
+        self.verbose = verbose
 
     def decode(self, sequence, model):
+        """Given a sequence and a model, generate a summary according to
+        beam search."""
+
         # Initalize the summary with enough starting tags
         summary = [self.word2idx['<s>']] * self.C
         sequence = Variable(sequence)
@@ -56,9 +63,10 @@ class Beam_Search_Decoder(Decoder):
         probs, indices = self.predict(model, sequence, summary[:self.C])
         hypotheses = [(summary + [indices[0][i]], probs[0][i])
                       for i in range(self.beam_size)]
-        # print("Top K")
-        # for hypothesis in hypotheses:
-        #     self.print_hypothesis(hypothesis)
+        if self.verbose:
+            print("Top K")
+            for hypothesis in hypotheses:
+                self.print_hypothesis(hypothesis)
 
         # For every index in summary, reestimate top K best hypotheses
         for i in range(self.C+1, self.length):
@@ -72,18 +80,22 @@ class Beam_Search_Decoder(Decoder):
                 for k in range(self.beam_size):
                     token = indices[0][k]
                     new_prob = prob + probs[0][k]
+
+                    # Only keep the best hypothesis per continuation
                     if ((token not in n_h) or
                        (token in n_h and new_prob > n_h[token][1])):
                         n_h[token] = (hypothesis + [token], new_prob)
 
-            # print("New Hypotheses")
-            # for key in n_h:
-            #     self.print_hypothesis(n_h[key])
             # Select top K hypotheses from new_hypotheses
             hypotheses = self.select_top(list(n_h.values()), self.beam_size)
-            # print("Top K")
-            # for hypothesis in hypotheses:
-            #     self.print_hypothesis(hypothesis)
+
+            if self.verbose:
+                print("New Hypotheses")
+                for key in n_h:
+                    self.print_hypothesis(n_h[key])
+                print("Top K")
+                for hypothesis in hypotheses:
+                    self.print_hypothesis(hypothesis)
 
         # Indices to words
         summary, prob = self.select_top(hypotheses, 1)[0]
